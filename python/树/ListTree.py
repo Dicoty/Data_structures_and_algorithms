36bdc5a67a538c11d48727c698b45127c2ea621e
Dicoty <3064868730@qq.com>
2025-02-27 15:50:29 +0800

    first

---
+++ python/树/ListTree.py
@@ -0,0 +1,42 @@
+# 使用嵌套列表实现二叉树
+def ListBinaryTree(r):
+    return [r, [], []]
+
+def insertLeft(root, newBranch):
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1, [newBranch, t, []])
+    else:
+        root.insert(1, [newBranch, [], []])
+    return root
+
+def insertRight(root, newBranch):
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2, [newBranch, [], t])
+    else:
+        root.insert(2, [newBranch, [], []])
+    return root
+
+def getRootVal(root):
+    return root[0]
+
+def setRootVal(root, newVal):
+    root[0] = newVal
+
+def getLeftChild(root):
+    return root[1]
+
+def getRightChild(root):
+    return root[2]
+
+r = ListBinaryTree(3)
+insertLeft(r, 4)
+insertLeft(r, 5)
+insertRight(r, 6)
+insertRight(r, 7)
+l = getLeftChild(r)
+h = getRootVal(r)
+print(r)
+print(l)
+print(h)
